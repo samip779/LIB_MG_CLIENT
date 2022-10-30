2206a3a99ef13f85423dd8792c02ef5fbcb36be6
samip779 <samipluitel28@gmail.com>
2022-10-30 16:04:54 +0545

    added page "page not found" page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import { Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => {
   return (
-    <div className='min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-      <div className='max-w-md w-full space-y-8'>
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 };
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,18 +3,20 @@ import Login from '../components/Login';
 
 const LoginPage = () => {
   return (
-    <>
-      <div className='bg-gray-700 px-4 py-12 rounded-2xl shadow hover:shadow-2xl'>
-        <Header
-          heading='Login to your account'
-          paragraph="Don't have an account yet? "
-          linkName='Sign Up'
-          linkUrl='/signUp'
-        />
+    <div className='min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-md w-full space-y-8'>
+        <div className='bg-gray-700 px-4 py-12 rounded-2xl shadow hover:shadow-2xl'>
+          <Header
+            heading='Login to your account'
+            paragraph="Don't have an account yet? "
+            linkName='Sign Up'
+            linkUrl='/signUp'
+          />
 
-        <Login />
+          <Login />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
